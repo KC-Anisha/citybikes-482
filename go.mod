08584db7b50a5f91a614f25f93c002ecf02495d8
KC-Anisha <anisha.kc7219@gmail.com>
2021-10-13 18:32:25 -0400

    dynamodb write

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module citybikes-482
 
 go 1.17
 
-require github.com/JamesPEarly/loggly v0.0.0-20190206220007-041558ec1c7c
+require (
+	github.com/JamesPEarly/loggly v0.0.0-20190206220007-041558ec1c7c
+	github.com/aws/aws-sdk-go v1.41.1
+	github.com/google/uuid v1.3.0
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -2,35 +2,46 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
-	"strconv"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"time"
+	"strconv"
 	loggly "github.com/JamesPEarly/loggly"
+	"time"
+	"log"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/google/uuid"
 )
+
+type Item struct {
+	Time    string
+	Id string
+	Stations []Station
+}
 
 type Network struct {
 	Stations Stations `json:"network"`
 }
 
 type Stations struct {
-    Stations []Station `json:"stations"`
+	Stations []Station `json:"stations"`
 }
 
 type Station struct {
-    EmptySlots   int `json:"empty_slots"`
-    FreeBikes   int `json:"free_bikes"`
-    Name    string    `json:"name"`
-    Extra Extra `json:"extra"`
-	Id string `json:"id"`
+	EmptySlots int    `json:"empty_slots"`
+	FreeBikes  int    `json:"free_bikes"`
+	Name       string `json:"name"`
+	Extra      Extra  `json:"extra"`
+	Id         string `json:"id"`
 }
 
 type Extra struct {
-    Renting int `json:"renting"`
-    Returning  int `json:"returning"`
+	Renting   int `json:"renting"`
+	Returning int `json:"returning"`
 }
-
 
 func pollData() {
 	// Tag + client init for Loggly
@@ -50,26 +61,59 @@ func pollData() {
 		client.EchoSend("error", "Failed with error: " + err.Error())
 	}
 
-
 	// Parse the JSON and display some info to the terminal
 	var network Network
 	json.Unmarshal(body, &network)
-
 	formattedData, _ := json.MarshalIndent(network, "", "    ")
 	fmt.Println(string(formattedData))
 
-	
 	// Send success message to loggly with response size
 	var respSize string = strconv.Itoa(len(body))
 	logErr := client.EchoSend("info", "Successful data collection of size: " + respSize)
 	if (logErr != nil) {
 		fmt.Println("err: ", logErr)
 	}
+
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	// Create item to be added to DynamoDB
+	var item Item
+	id := uuid.New().String()
+	item.Stations = network.Stations.Stations
+	item.Id = id
+	item.Time = time.Now().Format(time.RFC3339)
+
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		log.Fatalf("Got error marshalling new network item: %s", err)
+	}
+
+	// Create item in table citybikes
+	tableName := "citybikes"
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Fatalf("Got error calling PutItem: %s", err)
+	}
+
+	fmt.Println("Successfully added to table " + tableName)
 }
 
-
 func main() {
-	for range time.Tick(time.Minute * 30) {
-        pollData()
-    }
+	// for range time.Tick(time.Minute * 30) {
+	pollData()
+	// }
 }
